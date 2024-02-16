0ad459ac93f50183f0dc6c6623e25160f1febccb
Aman Raj <42941876+amanrajtoppoart007@users.noreply.github.com>
2024-02-16 16:47:51 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import authRoutes from './routes/auth-routes'
-import appRoutes from './routes/app-routes'
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import authRoutes from "./routes/auth-routes";
+import appRoutes from "./routes/app-routes";
 
 function App() {
-  const token = localStorage.getItem('loggedIn')
+  const token = localStorage.getItem("loggedIn");
+  console.log(token);
   return (
     <>
-    <RouterProvider router={token === 'yes' ? appRoutes : authRoutes}/>
+      <RouterProvider router={token === "yes" ? appRoutes : authRoutes} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
